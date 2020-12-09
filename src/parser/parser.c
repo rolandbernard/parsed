89375e301dd63e79f649fbb751c93fe0dc8da79d
Roland Bernard <rolli.bernard@gmail.com>
2020-12-09 21:36:35 +0100

    Added the files for the scanner

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -1,8 +1,6 @@
 
 #include "parser.h"
 
-
-
 Ast* parseGrammar(const char* src, ErrorContext* error_context) {
     return NULL;
 }
---
+++ src/parser/scanner.h
@@ -0,0 +1,8 @@
+#ifndef _SCANNER_H_
+#define _SCANNER_H_
+
+typedef struct {
+    
+} Scanner;
+
+#endif
