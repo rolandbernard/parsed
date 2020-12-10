2e48e800600e0a2f6034f10964f442cd528f6e30
Roland Bernard <rolli.bernard@gmail.com>
2020-12-10 21:52:50 +0100

    Started work on the parser

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -1,6 +1,42 @@
 
 #include "parser.h"
+#include "scanner.h"
+
+#define PARSER_ERROR (void*)1
+
+Ast* parseRootElement(Scanner* scanner, ErrorContext* error_context) {
+    return NULL;
+}
+
+AstRoot* parseRoot(Scanner* scanner, ErrorContext* error_context) {
+    AstRoot* ret = createAstRoot();
+    Ast* child = NULL;
+    while((child = parseRootElement(scanner, error_context)) != NULL) {
+        if(child == PARSER_ERROR) {
+            freeAst((Ast*)ret);
+            return PARSER_ERROR;
+        } else {
+            addChildToAstRoot(ret, child);
+        }
+    }
+    return ret;
+}
 
 Ast* parseGrammar(const char* src, ErrorContext* error_context) {
-    return NULL;
+    Scanner scanner;
+    initScanner(&scanner, src);
+    AstRoot* ret = parseRoot(&scanner, error_context);
+    if(ret == PARSER_ERROR) {
+        freeScanner(&scanner);
+        return NULL;
+    } else if(!acceptToken(&scanner, TOKEN_EOF, NULL)) {
+        freeAst(ret);
+        addError(error_context, "Expected end of file", getOffsetOfNextToken(&scanner), ERROR);
+        freeScanner(&scanner);
+        freeAst((Ast*)ret);
+        return NULL;
+    } else {
+        freeScanner(&scanner);
+        return ret;
+    }
 }
