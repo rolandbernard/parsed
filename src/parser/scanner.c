a1476a5f679166f8e6c8a2f836a91022a371c173
Roland Bernard <rolli.bernard@gmail.com>
2020-12-10 21:06:00 +0100

    Implemented a scanner

--- src/parser/scanner.c
+++ src/parser/scanner.c
@@ -1,3 +1,109 @@
 
+#include <ctype.h>
+#include <stddef.h>
+
 #include "scanner.h"
 
+void initScanner(Scanner* scanner, const char* src) {
+    scanner->src = src;
+    scanner->offset = 0;
+    scanner->is_cached = false;
+}
+
+static int determenNextToken(const char* src, TokenType* out) {
+    int len = 1;
+    *out = TOKEN_INVALID;
+    if(isalnum(src[0]) || src[0] == '_') {
+        while (isalnum(src[len]) || src[len] == '_') {
+            len++;
+        }
+        *out = TOKEN_IDENTIFIER;
+    } else if(src[0] == '{') {
+        int nested = 0;
+        while (src[len] != 0 && (nested != 0 || src[len] != '}')) {
+            if(src[len] == '{') {
+                nested++;
+            } else if(src[len] == '}') {
+                nested--;
+            }
+            len++;
+        }
+        if(src[len] == 0) {
+            *out = TOKEN_UNCLOSED_C_SOURCE;
+        } else {
+            len++;
+            *out = TOKEN_C_SOURCE;
+        }
+    } else if(src[0] == '"' || src[0] == '\'') {
+        while (src[len] != 0 && src[len] != src[0]) {
+            if(src[len] == '\\') {
+                len++;
+            }
+            len++;
+        }
+        if(src[len] == 0) {
+            *out = TOKEN_UNCLOSED_STRING;
+        } else {
+            len++;
+            *out = TOKEN_STRING_TOKEN;
+        }
+    } else if(src[0] == '/') {
+        while (src[len] != 0 && src[len] != '/') {
+            if(src[len] == '\\') {
+                len++;
+            }
+            len++;
+        }
+        if(src[len] == 0) {
+            *out = TOKEN_UNCLOSED_REGEX;
+        } else {
+            len++;
+            *out = TOKEN_REGEX_TOKEN;
+        }
+    } else if(src[0] == '%') {
+        *out = TOKEN_PERCENT;
+    } else if(src[0] == '|') {
+        *out = TOKEN_ALTERNATIVE;
+    } else if(src[0] == ':' && src[1] == '=') {
+        len = 2;
+        *out = TOKEN_DEFINE;
+    }
+    return len;
+}
+
+Token getNextToken(Scanner* scanner) {
+    if(!scanner->is_cached) {
+        TokenType type;
+        int len = determenNextToken(scanner->src + scanner->offset, &type);
+        scanner->cached.start = scanner->src + scanner->offset;
+        scanner->cached.len = len;
+        scanner->cached.offset = scanner->offset;
+        scanner->cached.type = type;
+        scanner->offset += len;
+        scanner->is_cached = true;
+    }
+    return scanner->cached;
+}
+
+Token consumeNextToken(Scanner* scanner) {
+    getNextToken(scanner);
+    scanner->is_cached = false;
+    return scanner->cached;
+}
+
+bool acceptToken(Scanner* scanner, TokenType type, Token* out) {
+    getNextToken(scanner);
+    if(out != NULL) {
+        *out = scanner->cached;
+    }
+    if(scanner->cached.type == type) {
+        scanner->is_cached = false;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void freeScanner(Scanner* scanner) {
+    // Nothing to free
+}
--- src/parser/scanner.h
+++ src/parser/scanner.h
@@ -1,8 +1,44 @@
 #ifndef _SCANNER_H_
 #define _SCANNER_H_
 
+#include <stdbool.h>
+
+typedef enum {
+    TOKEN_INVALID,
+    TOKEN_IDENTIFIER,
+    TOKEN_STRING_TOKEN,
+    TOKEN_REGEX_TOKEN,
+    TOKEN_DEFINE,
+    TOKEN_ALTERNATIVE,
+    TOKEN_C_SOURCE,
+    TOKEN_PERCENT,
+    TOKEN_UNCLOSED_STRING,
+    TOKEN_UNCLOSED_REGEX,
+    TOKEN_UNCLOSED_C_SOURCE,
+} TokenType;
+
 typedef struct {
-    
+    TokenType type;
+    const char* start;
+    int offset;
+    int len;
+} Token;
+
+typedef struct {
+    const char* src;
+    int offset;
+    bool is_cached;
+    Token cached;
 } Scanner;
+
+void initScanner(Scanner* scanner, const char* src);
+
+Token getNextToken(Scanner* scanner);
+
+Token consumeNextToken(Scanner* scanner);
+
+bool acceptToken(Scanner* scanner, TokenType type, Token* out);
+
+void freeScanner(Scanner* scanner);
 
 #endif
